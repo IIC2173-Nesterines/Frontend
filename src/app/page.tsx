1c82328948d5dee169a9c5ecc7af799171aceab7
Diego Sfeir <dsfeir@uc.cl>
2024-04-17 21:53:03 -0400

    feat: NavBar Created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import {
   Button, Typography, Box,
 } from '@mui/material';
 import FlightsDashboard from '@/components/FlightsDashboard';
+import NavBar from '@/components/NavBar';
 
 export default function Home() {
   const { user, isLoading } = useUser();
@@ -15,25 +16,12 @@ export default function Home() {
     <main className="flex h-screen w-full flex-col bg-white justify-center">
       {user ? (
         <Box className="flex w-full h-full flex-col items-center mt-6 gap-8 justify-center mt-16">
-          <Typography variant="body1" className="text-black">
-            Welcome
-            {' '}
-            {user?.name || ''}
-            {' '}
-            !
-          </Typography>
-          <Button variant="contained" href="/api/auth/logout" className="w-1/6">Sign out</Button>
+          <NavBar />
           <FlightsDashboard />
         </Box>
       ) : (
-        <Box
-          alignItems="center"
-          display="flex"
-          flexDirection="column"
-          gap={2}
-        >
-          <Typography variant="body1" className="text-black">You are not logged in</Typography>
-          <Button variant="contained" href="/api/auth/login">Log in</Button>
+        <Box className="flex w-full h-full flex-col items-center mt-6 gap-8 mt-16">
+          <NavBar />
         </Box>
       )}
     </main>

6ebdb9042ed11ebf69516eaba018f77838a8fe10
Daniel-SebastianGaete <daniel.sebastian@uc.cl>
2024-05-23 15:49:39 -0400

    debug

--- src/api/flight.api.ts
+++ src/api/flight.api.ts
@@ -11,7 +11,7 @@ export const FlightAPI = {
   getFlights: (amount:number, page:number) => axiosInstance.get(`${viewFlights}?amount=${amount}&page=${page}`),
   getAllFlights: () => axiosInstance.get(viewFlights),
   getFlight: (id:number) => axiosInstance.get(`${viewFlights}/${id}`),
-  getAllTickets: (sessionId: string) => axiosInstance.get(`${viewTickets}/${sessionId}`),
+  getAllTickets: (sessionId: string) => axiosInstance.get(encodeURIComponent(`${viewTickets}/${sessionId}`)),
   bookFlight: (data: RequestType) => axiosInstance.post(viewRequests, data),
   getUpcomingFlights: (data: upcomingFlightsType) => axiosInstance.post(`${viewFlights}/upcoming`, data),
   generateRecommendations: (data: RecType, sessionId:string) => axiosInstance.post(encodeURIComponent(`${viewRecommendations}/${sessionId}`), data),
